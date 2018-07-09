d3ab2487c9dfe2f5973eefd7dab6061d556a8bca
moisesmcardona <moises@moises-studios.com>
2018-07-09 15:57:22 -0400

    Added Google Drive components and started coding the class.

---
+++ DLNACore/GoogleDriveClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Google.Apis.Download;
+namespace DLNAPlayer
+{
+    class GoogleDriveClass
+    {
+        static string[] Scopes = { DriveService.Scope.DriveFile, DriveService.Scope.Drive };
+        static string SoftwareName = "DLNA Player";
+        public DriveService service;
+    }
+    
+}
